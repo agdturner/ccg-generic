317c4fb0a2688466577c71d860568390f667a711
geoagdt <geoagdt@GEO-J20.ds.leeds.ac.uk>
2016-07-18 18:11:11 +0100

    Very early work reading CSV files...

--- src/main/java/uk/ac/leeds/ccg/andyt/generic/io/Generic_ReadCSV.java
+++ src/main/java/uk/ac/leeds/ccg/andyt/generic/io/Generic_ReadCSV.java
@@ -35,9 +35,10 @@ import java.util.logging.Logger;
  */
 public class Generic_ReadCSV {
  
-    public static boolean testReadStreamTokenizerSyntax1x(
+    public static boolean testRead(
             File f,
-    File testDir) {
+    File testDir,
+    int syntax) {
         
         ArrayList<String> result = null;
         
@@ -58,7 +59,23 @@ public class Generic_ReadCSV {
                 if (br != null) {
                     result = new ArrayList<String>();
                     st = new StreamTokenizer(br);
-                    Generic_StaticIO.setStreamTokenizerSyntax1(st);
+                    switch (syntax) {
+                        case 1:
+                            Generic_StaticIO.setStreamTokenizerSyntax1(st);
+        break;
+                        case 2:
+                            Generic_StaticIO.setStreamTokenizerSyntax2(st);
+        break;
+                        case 3:
+                            Generic_StaticIO.setStreamTokenizerSyntax3(st);
+        break;
+                        case 4:
+                            Generic_StaticIO.setStreamTokenizerSyntax4(st);
+        break;
+                        default:
+                            Generic_StaticIO.setStreamTokenizerSyntax5(st);
+        break;
+                    }
                     int token = st.nextToken();
 //                    long RecordID = 0;
                     String line = "";
@@ -103,72 +120,5 @@ public class Generic_ReadCSV {
         }
         //return result;
     }
-    
-    public static boolean testReadStreamTokenizerSyntax1(
-            File f,
-    File testDir) {
-        
-        ArrayList<String> result = null;
-        
-        
-        File test;
-        test = new File("test.csv");
-        PrintWriter pw = null;
-        try {
-            pw = new PrintWriter(test);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Generic_ReadCSV.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (f.exists()) {
-            try {
-                BufferedReader br;
-                StreamTokenizer st;
-                br = Generic_StaticIO.getBufferedReader(f);
-                if (br != null) {
-                    result = new ArrayList<String>();
-                    st = new StreamTokenizer(br);
-                    Generic_StaticIO.setStreamTokenizerSyntax1(st);
-                    int token = st.nextToken();
-//                    long RecordID = 0;
-                    String line = "";
-                    while (!(token == StreamTokenizer.TT_EOF)) {
-                        switch (token) {
-                            case StreamTokenizer.TT_EOL:
-//                                if (RecordID % 100 == 0) {
-//                                    System.out.println(line);
-//                                }
-//                                RecordID++;
-                                break;
-                            case StreamTokenizer.TT_WORD:
-                                line = st.sval;
-                                result.add(line);
-                                pw.println(line);
-                                break;
-                        }
-                        token = st.nextToken();
-                    }
-                    br.close();
-                }
-                pw.close();
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
-        }
-        
-        long length0;
-        length0 = test.length();
-        long length1;
-        length1 = f.length();
-        
-        if (length0 == length1) {
-            System.out.println("length0 == length1");
-            return true;
-        } else {
-            return false;
-        }
-        //return result;
-    }
-    
-    
     
 }
