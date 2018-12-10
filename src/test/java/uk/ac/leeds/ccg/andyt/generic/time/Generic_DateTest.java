d9f58c9ee5f3313689193d35fe56dd96394cb3c2
Andy Turner <a.g.d.turner@leeds.ac.uk>
2018-12-10 18:38:53 +0000

    Clean up and release. This was a major refactoring to split generic into bits. What remains is the most general part.

--- src/test/java/uk/ac/leeds/ccg/andyt/generic/time/Generic_DateTest.java
+++ src/test/java/uk/ac/leeds/ccg/andyt/generic/time/Generic_DateTest.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
  */
-package uk.ac.leeds.ccg.andyt.generic.utilities.time;
+package uk.ac.leeds.ccg.andyt.generic.time;
 
 import java.io.File;
 import org.junit.After;
