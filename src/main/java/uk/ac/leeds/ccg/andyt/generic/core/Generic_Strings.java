d194cc0976f8e21e67e6630bc74c66aac7f58a28
Andy Turner <a.g.d.turner@leeds.ac.uk>
2019-02-26 12:09:45 +0000

    Simplifications for Strings.

--- src/main/java/uk/ac/leeds/ccg/andyt/generic/core/Generic_Strings.java
+++ src/main/java/uk/ac/leeds/ccg/andyt/generic/core/Generic_Strings.java
@@ -22,91 +22,11 @@ package uk.ac.leeds.ccg.andyt.generic.core;
  * @version 1.0.0
  */
 public class Generic_Strings {
-
-    // Strings starting with a capital letter
-    public static final String s_BigDecimal = "BigDecimal";
-    public static final String s_BigInteger = "BigInteger";
-    public static final String s_Byte = "Byte";
-    public static final String s_DATA = "data";
-    public static final String s_Data = "Data";
-    public static final String s_Double = "Double";
-    public static final String s_Float = "Float";
-    public static final String s_HashMap = "HashMap";
-    public static final String s_HashSet = "HashSet";
-    public static final String s_Integer = "Integer";
-    public static final String s_Long = "Long";
-    public static final String s_String = "String";
-    public static final String s_Short = "Short";
-
-    // Strings starting with a lower case letter
-    public static final String s_byte = "byte";
-    public static final String s_dat = "dat";
-    public static final String s_data = "data";
-    public static final String s_double = "double";
-    public static final String s_float = "float";
-    public static final String s_generated = "generated";
-    public static final String s_log = "log";
-    public static final String s_input = "input";
-    public static final String s_int = "int";
-    public static final String s_long = "long";
-    public static final String s_output = "output";
-    public static final String s_short = "short";
-    // Alphabets
-    // Upper case
-    public static final String s_A = "A";
-    public static final String s_B = "B";
-    public static final String s_C = "C";
-    public static final String s_D = "D";
-    public static final String s_E = "E";
-    public static final String s_F = "F";
-    public static final String s_G = "G";
-    public static final String s_H = "H";
-    public static final String s_I = "I";
-    public static final String s_J = "J";
-    public static final String s_K = "K";
-    public static final String s_L = "L";
-    public static final String s_M = "M";
-    public static final String s_N = "N";
-    public static final String s_O = "O";
-    public static final String s_P = "P";
-    public static final String s_Q = "Q";
-    public static final String s_R = "R";
-    public static final String s_S = "S";
-    public static final String s_T = "T";
-    public static final String s_U = "U";
-    public static final String s_V = "V";
-    public static final String s_W = "W";
-    public static final String s_X = "X";
-    public static final String s_Y = "Y";
-    public static final String s_Z = "Z";
-    // Lower case
-    public static final String s_a = "a";
-    public static final String s_b = "b";
-    public static final String s_c = "c";
-    public static final String s_d = "d";
-    public static final String s_e = "e";
-    public static final String s_f = "f";
-    public static final String s_g = "g";
-    public static final String s_h = "h";
-    public static final String s_i = "i";
-    public static final String s_j = "j";
-    public static final String s_k = "k";
-    public static final String s_l = "l";
-    public static final String s_m = "m";
-    public static final String s_n = "n";
-    public static final String s_o = "o";
-    public static final String s_p = "p";
-    public static final String s_q = "q";
-    public static final String s_r = "r";
-    public static final String s_s = "s";
-    public static final String s_t = "t";
-    public static final String s_u = "u";
-    public static final String s_v = "v";
-    public static final String s_w = "w";
-    public static final String s_x = "x";
-    public static final String s_y = "y";
-    public static final String s_z = "z";
-    // Symbols
+    // Special Strings
+    public static final String special_emptyString = "";
+    public static final String special_newLine = "\n";
+    public static final String special_commaSpace = ", ";
+    // Commonly used symbols
     public static final String symbol_0 = "0";
     public static final String symbol_1 = "1";
     public static final String symbol_2 = "2";
@@ -128,11 +48,88 @@ public class Generic_Strings {
     public static final String symbol_questionmark = "?";
     public static final String symbol_space = " ";
     public static final String symbol_underscore = "_";
-    // Specials
-    public static final String special_emptyString = "";
-    public static final String special_newLine = "\n";
-    public static final String special_commaSpace = ", ";
-
+    // Alphabetical list of commonly used word type Strings
+    public static final String s_A = "A";
+    public static final String s_a = "a";
+    public static final String s_ArrayList = "ArrayList";
+    public static final String s_B = "B";
+    public static final String s_b = "b";
+    public static final String s_BigDecimal = "BigDecimal";
+    public static final String s_BigInteger = "BigInteger";
+    public static final String s_Byte = "Byte";
+    public static final String s_byte = "byte";
+    public static final String s_C = "C";
+    public static final String s_D = "D";
+    public static final String s_c = "c";
+    public static final String s_d = "d";
+    public static final String s_dat = "dat";
+    public static final String s_DATA = "DATA";
+    public static final String s_Data = "Data";
+    public static final String s_data = "data";
+    public static final String s_Double = "Double";
+    public static final String s_double = "double";
+    public static final String s_E = "E";
+    public static final String s_e = "e";
+    public static final String s_f = "f";
+    public static final String s_F = "F";
+    public static final String s_Float = "Float";
+    public static final String s_float = "float";
+    public static final String s_G = "G";
+    public static final String s_g = "g";
+    public static final String s_generated = "generated";
+    public static final String s_H = "H";
+    public static final String s_h = "h";
+    public static final String s_HashMap = "HashMap";
+    public static final String s_HashSet = "HashSet";
+    public static final String s_I = "I";
+    public static final String s_i = "i";
+    public static final String s_input = "input";
+    public static final String s_int = "int";
+    public static final String s_Integer = "Integer";
+    public static final String s_J = "J";
+    public static final String s_j = "j";
+    public static final String s_K = "K";
+    public static final String s_k = "k";
+    public static final String s_L = "L";
+    public static final String s_l = "l";
+    public static final String s_log = "log";
+    public static final String s_Long = "Long";
+    public static final String s_long = "long";
+    public static final String s_M = "M";
+    public static final String s_m = "m";
+    public static final String s_N = "N";
+    public static final String s_n = "n";
+    public static final String s_O = "O";
+    public static final String s_o = "o";
+    public static final String s_output = "output";
+    public static final String s_P = "P";
+    public static final String s_p = "p";
+    public static final String s_Q = "Q";
+    public static final String s_q = "q";
+    public static final String s_R = "R";
+    public static final String s_r = "r";
+    public static final String s_S = "S";
+    public static final String s_s = "s";
+    public static final String s_String = "String";
+    public static final String s_Short = "Short";
+    public static final String s_short = "short";
+    public static final String s_T = "T";
+    public static final String s_t = "t";
+    public static final String s_TreeMap = "TreeMap";
+    public static final String s_True = "True";
+    public static final String s_U = "U";
+    public static final String s_u = "u";
+    public static final String s_V = "V";
+    public static final String s_v = "v";
+    public static final String s_W = "W";
+    public static final String s_w = "w";
+    public static final String s_X = "X";
+    public static final String s_x = "x";
+    public static final String s_Y = "Y";
+    public static final String s_y = "y";
+    public static final String s_Z = "Z";
+    public static final String s_z = "z";
+    
     public Generic_Strings() {
     }
 }
