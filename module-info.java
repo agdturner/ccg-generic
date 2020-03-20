d86912f45426b34597364d4b16bd03beca4939b2
Andy Turner <A.G.D.Turner@leeds.ac.uk>
2020-03-20 18:21:02 +0000

    modularisation

---
+++ module-info.java
@@ -0,0 +1,12 @@
+module uk.leeds.ccg.generic {
+    requires java.logging;
+    requires java.desktop;
+    exports uk.ac.leeds.ccg.generic.core;
+    exports uk.ac.leeds.ccg.generic.execution;
+    exports uk.ac.leeds.ccg.generic.io;
+    exports uk.ac.leeds.ccg.generic.lang;
+    exports uk.ac.leeds.ccg.generic.math;
+    exports uk.ac.leeds.ccg.generic.memory;
+    exports uk.ac.leeds.ccg.generic.time;
+    exports uk.ac.leeds.ccg.generic.visualisation;
+}
