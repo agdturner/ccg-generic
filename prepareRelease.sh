7930e01764ea5754d44742a08865e43aebf0cc8d
Andy Turner <a.g.d.turner@leeds.ac.uk>
2020-11-30 17:39:55 +0000

    Release of version 1.7.1 updating junit dependency version.

--- prepareRelease.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-read -sp "Enter passphrase: " pass
-cp pom.xml target/agdt-java-generic-1.0.0-pom.xml
-cd target/
-echo "${pass}" | gpg --batch --no-tty --yes --passphrase-fd 0 --symmetric -o agdt-java-generic-1.0.0-pom.xml.asc agdt-java-generic-1.0.0-pom.xml
-echo "${pass}" | gpg --batch --no-tty --yes --passphrase-fd 0 --symmetric -o agdt-java-generic-1.0.0.jar.asc agdt-java-generic-1.0.0.jar
-echo "${pass}" | gpg --batch --no-tty --yes --passphrase-fd 0 --symmetric -o agdt-java-generic-1.0.0-javadoc.jar.asc agdt-java-generic-1.0.0-javadoc.jar
-echo "${pass}" | gpg --batch --no-tty --yes --passphrase-fd 0 --symmetric -o agdt-java-generic-1.0.0-sources.jar.asc agdt-java-generic-1.0.0-sources.jar
